840661f17f795d4dcb471e9a82259e4ca0208b32
mahmoud123force <mahmoudrarehp12312@gmail.com>
2025-08-07 22:48:05 +0300

    Add files via upload

---
+++ madelips.py
@@ -0,0 +1,31 @@
+name=str(input("my name is:"))
+age=int(input("my age is:"))
+if age < 18:
+    print("im not an adult so iam not allowed to vote nor drive nor drink alcohol and i cant work more than 4 hours a day but i can go to school and make friendship and play games it is advantageous to be a child and disadvantageous to be a child at the same way their are so many people who whish to be children so they can build their own future and be happy and free from all the responsibilities of life ")
+else:
+    print("im an adult so iam allowed to vote and drive and drink alcohol and i can work more than 4 hours a day but i cant go to school and make friendship and play games it is disadvantageous to be a child and advantageous to be a child at the same way their are so many people who whish to be adults so they can build their own future and be happy and free from all the responsibilities of life , i can work in more jobs and earn more money and have more freedom but i have to take care of my family and pay bills and taxes and face the challenges of life ")
+
+    hobbies = str(input("my hobbies are:"))
+    print("i learned this hobbies from my parents and friends and i enjoy doing them in my free time and they make me happy and relaxed and i can also share them with others and learn new things from them and improve my skills and knowledge and creativity and express myself and have fun and make memories and experiences that i will cherish for the rest of my life ")
+    sleeping_hours = int(input("my sleeping hours are:"))
+    if sleeping_hours < 7:
+        print("i need to sleep more to be healthy and productive and avoid stress and fatigue and improve my mood and performance and memory and concentration and immune system and overall well-being ")
+    else:
+        print("i sleep enough to be healthy and productive and avoid stress and fatigue and improve my mood and performance and memory and concentration and immune system and overall well-being ")
+
+    favorite_food = str(input("my favorite food is:"))
+    print("i like this food because it tastes good and makes me feel satisfied and happy and gives me energy and nutrients and i can also share it with others and enjoy it together and try new recipes and cuisines and cultures and learn more about food and cooking and nutrition and health ")
+    favorite_color = str(input("my favorite color is:"))
+    print("i like this color because it makes me feel calm and relaxed and happy and it also reflects my personality and mood and style and i can also use it to express myself and my creativity and taste and preferences and i can also combine it with other colors and create new combinations and designs that suit my personality and mood ")
+    favorite_music = str(input("my favorite music is:"))
+    print("i like this music because it makes me feel good and relaxed and happy and it also reflects my personality and mood and style and i can also use it to express myself and my creativity and taste and preferences and i can also share it with others and enjoy it together and discover new artists and genres and cultures and learn more about music and its history and evolution ")
+    favorite_movie = str(input("my favorite movie is:"))
+    print("i like this movie because it tells a good story and has interesting characters and themes and messages and it also reflects my personality and mood and style and i can also use it to express myself and my creativity and taste and preferences and i can also share it with others and enjoy it together and discover new movies and genres and cultures and learn more about cinema and its history and evolution ")     
+    favorite_book = str(input("my favorite book is:"))
+    print("i like this book because it has a good plot and characters and themes and messages and it also reflects my personality and mood and style and i can also use it to express myself and my creativity and taste and preferences and i can also share it with others and enjoy it together and discover new books and genres and cultures and learn more about literature and its history and evolution ")
+    favorite_sport = str(input("my favorite sport is:"))
+    print("i like this sport because it is fun and exciting and challenging and it also reflects my personality and mood and style and i can also use it to express myself and my creativity and taste and preferences and i can also share it with others and enjoy it together and discover new sports and activities and cultures and learn more about sportsmanship and teamwork ")
+    favorite_animal = str(input("my favorite animal is:"))
+    print("i like this animal because it is cute and friendly and loyal and it also reflects my personality and mood and style and i can also use it to express myself and my creativity and taste and preferences and i can also share it with others and enjoy it together and discover new animals and species and cultures and learn more about nature and its diversity ")
+    favorite_place = str(input("my favorite place is:"))
+    print("i like this place because it is beautiful and peaceful and relaxing and it also reflects my personality and mood and style and i can also use it to express myself and my creativity and taste and preferences and i can also share it with others and enjoy it together and discover new places and cultures and learn more about geography and its history and evolution ")    
